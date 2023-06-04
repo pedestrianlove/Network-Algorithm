a91c441d723dbfb47eb24953d1c6adb73789d437
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2023-06-04 23:47:34 +0800

    Added basic d3.js code.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ require('~/main.css')
 // Import main layout
 import DefaultLayout from '~/layouts/Default.vue'
 
+// Import d3.js
+import * as d3 from "d3";
+
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
